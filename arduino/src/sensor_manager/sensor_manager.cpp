e2722b7f55dfb6293d55c989a8161b71a88c392c
AntteHeatta <94539209+AntteHeatta@users.noreply.github.com>
2024-10-12 15:35:57 +0300

    ESP8266 code for pressure sensor, minor hum-temp fixes

--- arduino/src/sensor_manager/sensor_manager.cpp
+++ arduino/src/sensor_manager/sensor_manager.cpp
@@ -1,15 +1,17 @@
 #include "sensor_manager.h"
 
-SensorManager::SensorManager() : humidityTemperatureSensor(5), luminositySensor(A0) {}
+SensorManager::SensorManager() : humidityTemperatureSensor(13), luminositySensor(A0), pressureSensor(0x76) {}
 
 void SensorManager::initialize()
 {
     humidityTemperatureSensor.begin();
+    pressureSensor.begin();
 }
 
 void SensorManager::readSensors()
 {
     humidityTemperatureSensor.read();
+    pressureSensor.read();
 }
 
 float SensorManager::getHumidity()
@@ -25,4 +27,9 @@ float SensorManager::getTemperature()
 float SensorManager::getLuminosity()
 {
     return luminositySensor.getLuminosity();
+}
+
+float SensorManager::getPressure()
+{
+    return pressureSensor.getPressure();
 }
